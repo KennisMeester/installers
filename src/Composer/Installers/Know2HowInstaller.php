0c4b36c938d304fc39027870961f89365b23f794
Erik Frèrejean <erikfrerejean@users.noreply.github.com>
2016-09-23 14:16:06 +0200

    Add custom profile location.

--- src/Composer/Installers/Know2HowInstaller.php
+++ src/Composer/Installers/Know2HowInstaller.php
@@ -3,8 +3,10 @@ namespace Composer\Installers;
 
 class Know2HowInstaller extends BaseInstaller
 {
+
     protected $locations = array(
-      'module' => 'modules/custom/know2how/{$name}',
+        'module' => 'modules/custom/know2how/{$name}',
+        'profile' => 'profiles/custom/{$name}',
     );
 
 }
